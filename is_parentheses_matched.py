f2d4d6a278a03beb2a2c0a4cc9297607b20c0e33
Feng <86672194+FengNg22@users.noreply.github.com>
2021-11-22 15:07:37 +0800

    Update is_parentheses_matched.py

--- is_parentheses_matched.py
+++ is_parentheses_matched.py
@@ -9,14 +9,21 @@ def is_parentheses_matched(myStr):
         if item in parentheses.keys():
             stack_open.append(item)
         elif item in parentheses.values():
+            # if there has opening braces, and the close braces is == to the corresponding latest open braces
             if len(stack_open) > 0 and item == parentheses.get(stack_open[-1]):
+                # remove the latest open braces since there has matched close braces
                 stack_open.pop()
             else:
                 return "Unbalanced"
-        if len(stack_open) == 0:
-            return 'Balanced'
+
+    if len(stack_open) == 0:
+        return 'Balanced'
+    else:
+        # for only has closed parenthesis
+        return "Unbalanced"
 
 
 print(is_parentheses_matched('{1+[2*3]+6}'))
 print(is_parentheses_matched('(1+3+(5*7])'))
 print(is_parentheses_matched('(([]))'))
+print(is_parentheses_matched('3+6-3+0)'))
