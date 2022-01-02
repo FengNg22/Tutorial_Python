5a5f8e70b9df93a18298cde27f00baab4c92dfcd
Feng <86672194+FengNg22@users.noreply.github.com>
2022-01-02 15:35:02 +0800

    Update is_parentheses_matched.py
    update with no parentheses condition

--- is_parentheses_matched.py
+++ is_parentheses_matched.py
@@ -5,22 +5,30 @@ def is_parentheses_matched(myStr):
         '[': ']'
     }
     stack_open = []
+    parentheses_exist= False
     for item in myStr:
         if item in parentheses.keys():
+            parentheses_exist= True
             stack_open.append(item)
         elif item in parentheses.values():
+            parentheses_exist = True
             # if there has opening braces, and the close braces is == to the corresponding latest open braces
             if len(stack_open) > 0 and item == parentheses.get(stack_open[-1]):
                 # remove the latest open braces since there has matched close braces
                 stack_open.pop()
             else:
-                return "Unbalanced"
+                return "Unbalancedd"
+
 
     if len(stack_open) == 0:
-        return 'Balanced'
+        if parentheses_exist:
+                return 'Balanced'
+        else:
+            return " No Parenthesis Exist"
     else:
-        # for only has closed parenthesis
+        # for only has open parenthesis
         return "Unbalanced"
+
 
 
 print(is_parentheses_matched('{1+[2*3]+6}'))
